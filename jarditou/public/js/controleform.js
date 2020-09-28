68a9c12aa3b39ad7253b929e95b35678e8250c86
Isabelle Bengrine <ibengrine@yahoo.com>
2020-09-28 22:57:50 +0200

    correction minor bugs in validation contact form

--- jarditou/public/js/controleform.js
+++ jarditou/public/js/controleform.js
@@ -6,108 +6,78 @@ document.getElementById("cpostal").addEventListener("input", checkcp);
 document.getElementById("email").addEventListener("input", checkemail);
 document.getElementById("votrequestion").addEventListener("input", checkquestion);
 
-//appeler fonction en cliqt sur envoyer dans form contact Jarditou - 
-//NB lors de l'utilisation de addEventListener ... le renvoi de false n'a aucun effet. L'interface dans la spécification dit que le type de retour est null. Utilisez event.preventDefault () à la place:
-//attention ici notre form est le [1] car ya champs de form dans barre de nav
-
-document.forms[1].addEventListener("submit", function(evenement){
-  if (document.getElementById("lname").value == "") {
-        evenement.preventDefault();
-        document.getElementById("lnameHelp").innerHTML = "Tapez un nom.";
-        document.getElementById("lname").focus();
-    } else if (document.getElementById("fname").value == "") {
-        evenement.preventDefault();
-        document.getElementById("fnameHelp").innerHTML = "Pensez à taper un prenom !";
-        document.getElementById("fname").focus();
-    } else if (document.getElementById("datenaissance").value == "") {
-      evenement.preventDefault();
-      document.getElementById("bdateHelp").innerHTML = "Pensez à taper une date de naissance !";
-      document.getElementById("datenaissance").focus();
-    } else if (document.getElementById("cpostal").value == "") {
-      evenement.preventDefault();
-      document.getElementById("cpHelp").innerHTML = "Pensez à taper un code postal !";
-      document.getElementById("cpostal").focus();
-    } else if (document.getElementById("email").value == "") {
-      evenement.preventDefault();
-      document.getElementById("emlHelp").innerHTML = "Pensez à taper une adresse e-mail !";
-      document.getElementById("email").focus();
-    } else if (document.getElementById("votrequestion").value == "") {
-      evenement.preventDefault();
-      document.getElementById("vqHelp").innerHTML = "Pensez à taper votre question !";
-      document.getElementById("votrequestion").focus();
-    }
-});
-
-
 //pour controler chaque champ
 function checknom(){
-  var x = document.forms["formulaire_contact"]["lname"].value;
-  if (x == "") {
+  var nom = document.forms["formulaire_contact"]["lname"].value;
+  if (nom.trim() == "") {
     document.getElementById("lnameHelp").innerHTML = "Attention, vous avez oublié de saisir votre nom!";
+    document.getElementById("lname").focus();
     return false;
   }else {
     var filtrenom = new RegExp("^[A-Za-zéêè' -]+$");
-    var resultatnom = filtrenom.test(x);
+    var resultatnom = filtrenom.test(nom);
     if (resultatnom == false){
-      document.getElementById("lnameHelp").innerHTML = "Vérifiez votre nom!";
+      document.getElementById("lnameHelp").innerHTML = "Saisie invalide! Vérifiez votre saisie!";
       return false;
     }else {
-      document.getElementById("lnameHelp").innerHTML = "Nom OK!";
+      document.getElementById("lnameHelp").innerHTML = '<span class="text-success">Nom OK!</span>';
       return true;
     }
   }
 }
 
 function checkprenom(){
-  var y = document.forms["formulaire_contact"]["fname"].value;
-  if (y == "") {
+  var prenom = document.forms["formulaire_contact"]["fname"].value;
+  if (prenom.trim() == "") {
   document.getElementById("fnameHelp").innerHTML = "Attention, vous avez oublié d'entrer votre prénom!";
-    return false;
+  document.getElementById("fname").focus();
+  return false;
   }else {
     var filtreprenom = new RegExp("^[A-Za-zéêè' -]+$");
-    var resultatprenom = filtreprenom.test(y);
+    var resultatprenom = filtreprenom.test(prenom);
     if (resultatprenom == false){
-      document.getElementById("fnameHelp").innerHTML = "Vérifiez votre prénom!";
+      document.getElementById("fnameHelp").innerHTML = "Saisie invalide! Vérifiez votre saisie!";
       return false;
     }else {
-      document.getElementById("fnameHelp").innerHTML = "Prénom OK!";
+      document.getElementById("fnameHelp").innerHTML = '<span class="text-success">Prénom OK!</span>';
       return true;
     }
   }
 }
 
 function checkdatenaissance(){
-  var z = document.forms["formulaire_contact"]["datenaissance"].value;
-  if (z == "") {
+  var datenaiss = document.forms["formulaire_contact"]["datenaissance"].value;
+  if (datenaiss.trim() == "") {
     document.getElementById("bdateHelp").innerHTML = "Attention, vous avez oublié d'entrer votre date de naissance!";
+    document.getElementById("datenaissance").focus();
     return false;
   }else {
     var filtredn = new RegExp("^[0-3][0-9][/][0-1][0-9][/][0-9]{4}$");//format date ex 25/04/1973
-    var resultatdn = filtredn.test(z);
+    var resultatdn = filtredn.test(datenaiss);
     if (resultatdn == false){
       document.getElementById("bdateHelp").innerHTML = "Vérifiez votre date de naissance(format 'jj/mm/aaaa')!";
       return false;
     }else {
-      document.getElementById("bdateHelp").innerHTML = "Date de naissance OK!";
+      document.getElementById("bdateHelp").innerHTML = '<span class="text-success">Date de naissance OK!</span>';
       return true;
     }
   }
 }
 
 function checkcp(){
-    var d = document.forms["formulaire_contact"]["cpostal"].value;
-  if (d == "") {
+    var cp = document.forms["formulaire_contact"]["cpostal"].value;
+  if (cp.trim() == "") {
     document.getElementById("cpHelp").innerHTML = "Attention, vous avez oublié d'entrer votre code postal!";
+    document.getElementById("cpostal").focus();
     return false;
   }else {
-    var cpostal = document.getElementById("cpostal");
     var cpfiltre = new RegExp("^[0-9]{5}$");
-    var resultat = cpfiltre.test(cpostal.value);
+    var resultat = cpfiltre.test(cp);
     if (resultat == false) {
       document.getElementById("cpHelp").innerHTML = "Le Code Postal doit comporter 5 caractères numériques!";
       return false;
     }else {
-        document.getElementById("cpHelp").innerHTML = "Code postal OK!";
+        document.getElementById("cpHelp").innerHTML = '<span class="text-success">Code postal OK!</span>';
         return true;
     }
   }
@@ -115,18 +85,18 @@ function checkcp(){
 
 function checkemail(){
   var mail = document.forms["formulaire_contact"]["email"].value;
-  if (mail == "") {
+  if (mail.trim() == "") {
     document.getElementById("emlHelp").innerHTML = "Attention, vous avez oublié de préciser votre adresse e-mail!";
+    document.getElementById("email").focus();
     return false;
   }else {
-    var email = document.getElementById("email");
     var emailfiltre = new RegExp("^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]{2,}[.][a-z]{2,3}$");// regex pour email
-    var resultat = emailfiltre.test(email.value);
+    var resultat = emailfiltre.test(mail);
     if (resultat == false) {
       document.getElementById("emlHelp").innerHTML = "Attention! Vérifiez votre adresse e-mail, elle est invalide!";
       return false;
     }else {
-      document.getElementById("emlHelp").innerHTML = "Adresse e-mail valide!";
+      document.getElementById("emlHelp").innerHTML = '<span class="text-success">Adresse e-mail valide!</span>';
       return true;
     }
   }
@@ -134,12 +104,38 @@ function checkemail(){
 
 function checkquestion(){
   var vquestion = document.forms["formulaire_contact"]["votrequestion"].value;
-  if (vquestion == "") {
+  if (vquestion.trim() == "") {
     document.getElementById("vqHelp").innerHTML = "Attention, vous avez oublié de préciser votre question!";
+    document.getElementById("votrequestion").focus();
     return false;
   }else {
+    document.getElementById("vqHelp").innerHTML = '<span class="text-success">Question renseignée!</span>';
     return true;
   }
 }
 
+//appeler fonction en cliqt sur envoyer dans form contact Jarditou - 
+//attention ici notre form est le [1] car ya champs de form dans barre de nav
+
+document.forms[1].addEventListener("submit", function(evenement){
+  checknom();
+  checkprenom();
+  checkdatenaissance();
+  checkcp();
+  checkemail();
+  checkquestion();
+  if (checknom() == false) {
+    evenement.preventDefault();
+  } else if (checkprenom() == false) {
+    evenement.preventDefault();
+  } else if (checkdatenaissance() == false) {
+    evenement.preventDefault();
+  } else if (checkcp() == false) {
+    evenement.preventDefault();
+  } else if (checkemail() == false) {
+    evenement.preventDefault();
+  } else if (checkquestion() == false) {
+    evenement.preventDefault();
+  }
+});
 
